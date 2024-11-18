c7441ad5ac0428dbf2291dced99842790ee8975b
Erik Slovak <erik.slovak@technologystudio.sk>
2024-11-18 17:44:07 +0100

    ci: update release dry run workflow

---
+++ .releaserc.dryrun.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
+ * @Date: 2022-08-21T13:08:66+02:00
+ * @Copyright: Technology Studio
+**/
+
+module.exports = {
+  extends: [
+    './src/index.js',
+  ],
+  branches: [
+    process.env.BRANCH_NAME,
+  ]
+}
